337e0e1379954f8b8deb751344b7ad66a8ff255a
Grover101 <limachigrover09@gmail.com>
2023-05-25 08:14:14 -0400

    add view quizzes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import useToggle from './hooks/useToggle'
+
+import { Modal } from './components/Modal'
+import { Carousel } from './components/Carousel'
+
+import quizImg from './assets/quizzes.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [quizzes, setQuizzes] = useState(null)
+    const [selectQuiz, setSelectQuiz] = useState(null)
+    const { on, toggler } = useToggle()
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        const fecthData = async () => {
+            const res = await fetch('http://localhost:5173/db.json', {
+                method: 'GET'
+            })
+
+            const data = await res.json()
+            setQuizzes(data)
+        }
+
+        fecthData()
+    }, [])
+
+    const handleModal = index => {
+        toggler()
+        setSelectQuiz(index)
+    }
+
+    const updateQuizzes = (index, quiz) => {
+        const quizAux = quizzes
+        quizAux[index] = quiz
+        setQuizzes(quizAux)
+    }
+
+    return (
+        <>
+            {on && (
+                <Modal toggler={toggler}>
+                    <Carousel
+                        quizzes={quizzes}
+                        index={selectQuiz}
+                        update={updateQuizzes}
+                    />
+                </Modal>
+            )}
+            <div className="container mx-auto p-4">
+                {!quizzes ? (
+                    <div className="text-center mt-5">loading...</div>
+                ) : !quizzes.length ? (
+                    <div className="text-center mt-5">no hay cuestionario</div>
+                ) : (
+                    <div className="text-center mt-5">
+                        <h1 className="text-6xl">Videos Quiz</h1>
+                        <div className="grid grid-cols-4 gap-4 text-center mt-8">
+                            {quizzes.map(({ quiz }, index) => (
+                                <div key={index} className="mt-2">
+                                    <img
+                                        src={quiz.videoCover || quizImg}
+                                        className="m-auto rounded-xl cursor-pointer"
+                                        alt="portada"
+                                        width={200}
+                                        height={400}
+                                        onClick={() => handleModal(index)}
+                                    />
+                                    <p>{quiz}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )}
+            </div>
+        </>
+    )
 }
 
 export default App
